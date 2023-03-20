828c74355fc22edfc64d664ac76da842c409bf6e
mattbarnidge <mbarni109@gmail.com>
2023-03-20 12:55:19 -0500

    Update DJ RR Weights Robustness.R

--- INE/DJ RR Weights Robustness.R
+++ INE/DJ RR Weights Robustness.R
@@ -1,19 +1,16 @@
 rm3 = lmer(ipe ~ inv + 
-             age + female + poc + edu + inc + ideo + pid + 
-             sm.freq + size + div + grp + cur + 
+             age.c + female.c + poc.c + edu.c + inc.c + ideo.c + pid.c + 
+             sm.freq.c + size.c + div.c + grp.c +
              (1 | frame), 
            data=x, 
            control=lmerControl(optimizer="bobyqa"))
 
 rm4 = lmer(pol ~ inv +
-             age + female + poc + edu + inc + ideo + pid + 
-             sm.freq + size + div + grp + cur +
+             age.c + female.c + poc.c + edu.c + inc.c + ideo.c + pid.c + 
+             sm.freq.c + size.c + div.c + grp.c +
              (1 | frame), 
            data=x, 
            control=lmerControl(optimizer="bobyqa"))
 
-summary(lm1, cor=FALSE)
 summary(rm3, cor=FALSE)
-
-summary(lm2, cor=FALSE)
 summary(rm4, cor=FALSE)
